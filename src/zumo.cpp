3d26c53d38c4ce7af8a5b008b34eb37b11ffbca1
Neo11Neo <neobihli@gmail.com>
2024-09-09 10:17:49 +0200

    pid controller mit parameter und smoothing

--- src/zumo.cpp
+++ src/zumo.cpp
@@ -13,36 +13,29 @@ int error;
 int p;
 int i;
 int d;
-int lp;
+int lp = 0;
 int correction;
 
-const float Kp = 0;
-const float Ki = 0;
-const float Kd = 0;
+int light = 0;
+const float Kp = 1;
+const float Ki = 0.05;
+const float Kd = 2.5;
 
-int const max_speed;
-int const base_speed;
+int const max_speed = 400;
+int const min_speed = -400;
+int const base_speed = 300;
 
 int rspeed;
 int lspeed;
+int smoothed_rspeed = 0;
+int smoothed_lspeed = 0;
 
-bool run;
+bool run = true;
 
-void setup(){
+// Glättungsfaktor (zwischen 0 und 1)
+const float smoothing_factor = 0.95;
 
-}
-
-void loop(){
-
-    pid_calc();
-    motor_drive();
-
-}
-
-
-
-void pid_calc(){
-
+void pid_calc() {
     sensors.read(readings);
 
     error = readings[1] - readings[3];
@@ -52,33 +45,71 @@ void pid_calc(){
     d = p - lp;
     lp = p;
 
-    correction = int(Kp*p + Ki*i + Kd*d);
+    correction = int(Kp * p + Ki * i + Kd * d);
 
-    rspeed = constrain(base_speed + correction,0,max_speed);
-    lspeed = constrain(base_speed - correction,0,max_speed);
+    rspeed = constrain(base_speed + correction, min_speed, max_speed);
+    lspeed = constrain(base_speed - correction, min_speed, max_speed);
 
+    // Geschwindigkeit glätten
+    smoothed_rspeed = smoothed_rspeed + smoothing_factor * (rspeed - smoothed_rspeed);
+    smoothed_lspeed = smoothed_lspeed + smoothing_factor * (lspeed - smoothed_lspeed);
+
+    Serial.print(smoothed_rspeed);
+    Serial.print(" | ");
+    Serial.print(smoothed_lspeed);
+    Serial.print(" | ");
+    Serial.print(correction);
 }
 
-void motor_drive(int l,int r){
-    if(run) motors.setSpeeds(l,r);
+void motor_drive() {
+    Serial.print(" | ");
+    Serial.print(run);
+    Serial.print(" | ");
+    if (run) motors.setSpeeds(smoothed_lspeed, smoothed_rspeed);
+    else motors.setSpeeds(0, 0);
 }
 
-void fall_detect(){
-
+void fall_detect() {
+    // Implementiere die Fall-Erkennung
 }
 
-void no_line_detect(){
+void no_line_detect() {
+    light = 0;
+    for (int n = 0; n < 5; n++) {
+        Serial.print(readings[n]);
+        Serial.print(" | ");
+        light += readings[n];
+    }
 
+    Serial.println();
+
+    if (light < 1100) {
+        run = false;
+        light = 0;
+    } else {
+        run = true;
+    }
 }
 
-void finish_detect(){
-
+void finish_detect() {
+    // Implementiere die Ziel-Erkennung
 }
 
-void object_detect(){
-    
+void object_detect() {
+    // Implementiere die Objekt-Erkennung
 }
 
-void timer(){
+void timer() {
+    // Implementiere den Timer
+}
 
+void setup() {
+    sensors.initFiveSensors();
+    Serial.begin(115200);
+}
+
+void loop() {
+    pid_calc();
+    motor_drive();
+    no_line_detect();
 }
