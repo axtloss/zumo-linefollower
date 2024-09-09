e40900615ad4ab1e8ec235c0df479adc79c5e7c0
axtloss <axtlos@getcryst.al>
2024-09-09 13:42:42 +0200

    Add status display

--- src/zumo.cpp
+++ src/zumo.cpp
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Zumo32U4.h>
+#include "font.h"
 
 Zumo32U4LineSensors sensors;
 Zumo32U4Motors motors;
@@ -22,10 +23,8 @@ const float Kp = 1;
 const float Ki = 0.05;
 const float Kd = 9;
 
-int const max_speed_fwd = 400;
-int const min_speed_fwd = 230;
-int const max_speed_bwd = -400;
-int const min_speed_bwd = -230;
+int const max_speed = 400;
+int const min_speed = -400;
 int const base_speed = 400;
 
 int rspeed;
@@ -53,47 +52,19 @@ void pid_calc() {
 
     correction = int(Kp * p + Ki * i + Kd * d);
 
-    if (base_speed + correction > 0)
-      rspeed = constrain(base_speed + correction, min_speed_fwd, max_speed_fwd);
-    else if (base_speed + correction < 0)
-      rspeed = constrain(base_speed + correction, min_speed_bwd, max_speed_bwd);
-
-    if (base_speed - correction > 0)
-      lspeed = constrain(base_speed - correction, min_speed_fwd, max_speed_fwd);
-    else if (base_speed - correction < 0)
-      lspeed = constrain(base_speed - correction, min_speed_bwd, max_speed_bwd);
-
-    //rspeed = constrain(base_speed + correction, min_speed, max_speed);
-    //lspeed = constrain(base_speed - correction, min_speed, max_speed);
+    rspeed = constrain(base_speed + correction, min_speed, max_speed);
+    lspeed = constrain(base_speed - correction, min_speed, max_speed);
 
     // Geschwindigkeit glätten
     smoothed_rspeed = smoothed_rspeed + smoothing_factor * (rspeed - smoothed_rspeed);
     smoothed_lspeed = smoothed_lspeed + smoothing_factor * (lspeed - smoothed_lspeed);
 
-    if (smoothed_rspeed > 0 && smoothed_lspeed < 0) {
-      display.gotoXY(3,3);
-      display.print("<-");
-    } else if (smoothed_rspeed < 0 && smoothed_lspeed > 0) {
-      display.gotoXY(3,3);
-      display.print("->");
-    } else {
-      display.gotoXY(3,3);
-      display.print("/\\");
-    }
- 
-    if (curr_rate == update_rate) {
-      display.gotoXY(0,0);
-      display.println(smoothed_rspeed);
-      display.gotoXY(0,1);
-      display.println(smoothed_lspeed);
-      curr_rate = 0;
-    }
     Serial.print(smoothed_rspeed);
     Serial.print(" | ");
     Serial.print(smoothed_lspeed);
     Serial.print(" | ");
     Serial.print(correction);
-    curr_rate += 1;
+    //curr_rate += 1;
 }
 
 void motor_drive() {
@@ -138,27 +109,87 @@ void timer() {
     // Implementiere den Timer
 }
 
+
+void loadCustomCharactersFrwd()
+{
+  static const char levels[] PROGMEM = {
+    0, 0, 0, 0, 0, 0, 0, 63, 63, 63, 63, 63, 63, 63
+  };
+  display.loadCustomCharacter(levels + 0, 0);  // 1 bar
+  display.loadCustomCharacter(levels + 1, 1);  // 2 bars
+  display.loadCustomCharacter(levels + 2, 2);  // 3 bars
+  display.loadCustomCharacter(levels + 3, 3);  // 4 bars
+  display.loadCustomCharacter(levels + 4, 4);  // 5 bars
+  display.loadCustomCharacter(levels + 5, 5);  // 6 bars
+  display.loadCustomCharacter(levels + 6, 6);  // 7 bars
+}
+
+void loadCustomCharactersBkwd()
+{
+  static const char levels[] PROGMEM = {
+    63, 63, 63, 63, 63, 63, 63, 0, 0, 0, 0, 0, 0, 0
+  };
+  display.loadCustomCharacter(levels + 0, 6);  // 1 bar
+  display.loadCustomCharacter(levels + 1, 5);  // 2 bars
+  display.loadCustomCharacter(levels + 2, 4);  // 3 bars
+  display.loadCustomCharacter(levels + 3, 3);  // 4 bars
+  display.loadCustomCharacter(levels + 4, 2);  // 5 bars
+  display.loadCustomCharacter(levels + 5, 1);  // 6 bars
+  display.loadCustomCharacter(levels + 6, 0);  // 7 bars
+}
+
+
+void display_bar_frwd(int height, int x, int y) {
+  const char barChars[] = {' ', 0, 1, 2, 3, 4, 5, 6, (char)255};
+
+  loadCustomCharactersFrwd();
+  for (int i = 0; i <= 1; i++) {
+    display.gotoXY(x, y-i);
+    display.print(barChars[constrain(height-8*i, 0, 8)]);
+  }
+}
+
+void display_bar_bkwd(int height, int x, int y) {
+  const char barChars[] = {' ', 0, 1, 2, 3, 4, 5, 6, (char)255};
+
+  loadCustomCharactersBkwd();
+  for (int i = 0; i <= 1; i++) {
+    display.gotoXY(x, y+i);
+    display.print(barChars[constrain(height-8*i, 0, 8)]);
+  }
+}
+
+void display_direction() {
+  if (smoothed_lspeed < 0 && smoothed_rspeed > 0) {
+    display.gotoXY(4, 1);
+    display.print("<-");
+  } else if (smoothed_lspeed > 0 && smoothed_rspeed < 0) {
+    display.gotoXY(4, 1);
+    display.print("->");
+  } else if (smoothed_lspeed > 0 && smoothed_rspeed > 0) {
+    display.gotoXY(4, 1);
+    display.print("/\\");
+  } else if (smoothed_lspeed < 0 && smoothed_rspeed < 0) {
+    display.gotoXY(4, 1);
+    display.print("\\/");
+  } else {
+    display.gotoXY(4, 1);
+    display.print("  ");
+  }
+}
+
 void display_show() {
-    display.clear();
-    display.gotoXY(0, 0);
-    display.print("L:");
-    display.gotoXY(0, 1);
-    display.print("R:");
+  if (smoothed_lspeed > 0)
+    display_bar_frwd(run ? map(smoothed_lspeed, 0, max_speed, 0, 32) : 0, 0, 1);
+  else
+    display_bar_bkwd(run ? map(-smoothed_lspeed, 0, max_speed, 0, 32) : 0, 0, 2);
 
-    int lbar = map(smoothed_lspeed, min_speed, max_speed, 0, 8);
-    int rbar = map(smoothed_rspeed, min_speed, max_speed, 0, 8);
+  if (smoothed_rspeed > 0)
+    display_bar_frwd(run ? map(smoothed_rspeed, 0, max_speed, 0, 32) : 0, 10, 1);
+  else
+    display_bar_bkwd(run ? map(-smoothed_rspeed, 0, max_speed, 0, 32) : 0, 10, 2);
 
-    for (int i = 0; i < lbar; i++) {
-        display.gotoXY(2 + i, 0);
-        display.print((char)63); // Voller Block
-    }
-
-    for (int i = 0; i < rbar; i++) {
-        display.gotoXY(2 + i, 1);
-        display.print((char)63); // Voller Block
-    }
-
-    display.display();
+  display_direction();
 }
 
 void setup() {
@@ -173,5 +204,4 @@ void loop() {
     motor_drive();
     no_line_detect();
     display_show();
-    delay(300);
 }
